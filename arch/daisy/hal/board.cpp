8c519030b5259414469a8be7f9fc8827bb768b22
Jason <jaeysin@gmail.com>
2025-06-21 22:52:00 +0200

    Implement Daisy audio callback

--- arch/daisy/hal/board.cpp
+++ arch/daisy/hal/board.cpp
@@ -1,4 +1,5 @@
 #include <hal/board.h>
+#include <hal/constants.h>
 #include <hal/log.h>
 #include "daisy_seed.h"
 using namespace daisy;
@@ -13,6 +14,7 @@ void Board_init(void) {
     logInfo("Board_init: Daisy Seed");
     seed.Configure();
     seed.Init();
+    seed.SetAudioBlockSize(MAX_AUDIO_FRAME_LENGTH);
 }
 
 void Board_enableI2C2(void) {}
